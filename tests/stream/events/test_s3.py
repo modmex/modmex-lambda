2a8c818c571160be38aa29a9d892c10773f9db5f
Alejandro Hernández <clandro89@gmail.com>
2026-07-26 13:31:23 -0600

    Normalize an S3 EventBridge notification delivered through SQS

--- tests/stream/events/test_s3.py
+++ tests/stream/events/test_s3.py
@@ -1,7 +1,12 @@
 from expects import equal, expect
 import json
 
-from modmex_lambda.stream.events.s3 import from_s3, from_sqs_sns_s3, to_s3_records
+from modmex_lambda.stream.events.s3 import (
+    from_s3,
+    from_sqs_eventbridge_s3,
+    from_sqs_sns_s3,
+    to_s3_records,
+)
 
 
 def test_from_s3_created_events():
@@ -104,3 +109,53 @@ def test_from_sqs_sns_s3_expands_all_s3_records_from_each_message():
     expect([uow['event']['id'] for uow in uows]).to(
         equal(['one.json', 'two.json', 'one.json', 'two.json'])
     )
+
+
+def test_from_sqs_eventbridge_s3_normalizes_eventbridge_s3_notification():
+    eventbridge_record = {
+        'version': '0',
+        'id': 'eventbridge-id',
+        'detail-type': 'Object Created',
+        'source': 'aws.s3',
+        'account': '123456789012',
+        'time': '2026-07-26T18:35:42Z',
+        'region': 'us-east-1',
+        'resources': ['arn:aws:s3:::my-bucket'],
+        'detail': {
+            'version': '0',
+            'bucket': {'name': 'my-bucket'},
+            'object': {
+                'key': 'uploads/document.pdf',
+                'size': 123456,
+                'etag': 'd41d8cd98f00b204e9800998ecf8427e',
+                'sequencer': '0068A2D7B1F3D12345',
+            },
+            'request-id': 'ABCDEFG123456',
+            'requester': 'AIDA...',
+            'source-ip-address': '1.2.3.4',
+            'reason': 'PutObject',
+        },
+    }
+    sqs_record = {
+        'messageId': 'sqs-message-id',
+        'eventSource': 'aws:sqs',
+        'body': json.dumps(eventbridge_record),
+    }
+
+    uows = from_sqs_eventbridge_s3({'Records': [sqs_record]})
+
+    expect(len(uows)).to(equal(1))
+    expect(uows[0]['event']).to(equal({
+        'id': 'uploads/document.pdf',
+        'type': 'object-created',
+        's3': eventbridge_record['detail'],
+    }))
+    expect(uows[0]['record']).to(equal({
+        'sqs': sqs_record,
+        'eventbridge': eventbridge_record,
+        's3': {
+            **eventbridge_record,
+            'eventName': 'ObjectCreated:Put',
+            's3': eventbridge_record['detail'],
+        },
+    }))
